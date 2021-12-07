9a2a1fabcc10fc66e55a1e0b389a8d0a3c6e2997
vibinvj <vibinjayapal@gmail.com>
2021-12-08 04:30:59 +0530

    modules

--- dev/terraform.tfvars
+++ dev/terraform.tfvars
@@ -1,4 +1,4 @@
-sgname = "testsg"
+sgname = "devsg"
 ipblock = ["0.0.0.0/0", "10.0.1.0/24"]
 ins-tag = {
   name = "devinc"
@@ -7,6 +7,8 @@ ins-tag = {
 }
 devami = "ami-04ad2567c9e3d7893"
 pub_subnet = "10.0.1.0/24"
+pub_sub_2 = "10.0.3.0/24"
+pub_sub_3 = "10.0.4.0/24"
 ins_type = "t2.micro"
 sg-tag = {
   name = "devsg"
@@ -16,3 +18,25 @@ pub-sub-tag = {
   name = "pub-sub"
   env = "dev"
 }
+devvpc = "10.0.0.0/16"
+vpc-tag = {
+  name = "devvpc"
+  env = "dev"
+}
+pri_subnet = "10.0.2.0/24"
+pri_sub_tag = {
+  name = "pri_sub"
+  env = "dev"
+}
+devigw = {
+  name = "devigw"
+  env = "dev"
+}
+routetag = {
+  name = "routedev"
+  env = "dev"
+}
+pub_pri_Ip = "10.0.1.10"
+pri_pri_Ip = "10.0.2.10"
+vol_size = 1
+az_zone = "us-east-1a"
---
+++ module/VPC.tf
@@ -0,0 +1,117 @@
+resource "aws_vpc" "glb_vpc" {
+  cidr_block = var.devvpc
+  tags = var.vpc-tag
+}
+resource "aws_subnet" "glb_pub_sub" {
+  cidr_block = var.pub_subnet
+  vpc_id     = aws_vpc.glb_vpc.id
+  tags = var.pub-sub-tag
+  availability_zone = "us-east-1a"
+  depends_on = [aws_vpc.glb_vpc]
+}
+resource "aws_subnet" "glb_pub_sub_2" {
+  cidr_block = var.pub_sub_2
+  vpc_id     = aws_vpc.glb_vpc.id
+  tags = var.pub-sub-tag
+  availability_zone = "us-east-1c"
+  depends_on = [aws_vpc.glb_vpc]
+}
+resource "aws_subnet" "glb_pub_sub_3" {
+  cidr_block = var.pub_sub_3
+  vpc_id     = aws_vpc.glb_vpc.id
+  tags = var.pub-sub-tag
+  availability_zone = "us-east-1d"
+  depends_on = [aws_vpc.glb_vpc]
+}
+
+resource "aws_subnet" "glb_pri_sub" {
+  cidr_block = var.pri_subnet
+  vpc_id     = aws_vpc.glb_vpc.id
+  tags = var.pri_sub_tag
+  availability_zone = "us-east-1b"
+  depends_on = [aws_vpc.glb_vpc]
+}
+
+resource "aws_internet_gateway" "glb_igw" {
+  vpc_id = aws_vpc.glb_vpc.id
+  tags = var.devigw
+  depends_on = [aws_vpc.glb_vpc]
+}
+resource "aws_route_table" "glb_route" {
+  vpc_id = aws_vpc.glb_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.glb_igw.id
+  }
+  tags = var.routetag
+  depends_on = [aws_vpc.glb_vpc]
+}
+resource "aws_route_table_association" "glb_RT_attach" {
+  route_table_id = aws_route_table.glb_route.id
+  subnet_id = aws_subnet.glb_pub_sub.id
+}
+resource "aws_route_table_association" "glb_Rt-attach1" {
+  route_table_id = aws_route_table.glb_route.id
+  subnet_id = aws_subnet.glb_pub_sub_2.id
+}
+
+resource "aws_route_table_association" "glb_rt-attach2" {
+  route_table_id = aws_route_table.glb_route.id
+  subnet_id = aws_subnet.glb_pub_sub_3.id
+}
+
+resource "aws_route_table" "glb_pri_route" {
+  vpc_id = aws_vpc.glb_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.glb_nat.id
+  }
+  depends_on = [aws_vpc.glb_vpc]
+}
+resource "aws_route_table_association" "glb_pri_RT_attach" {
+  route_table_id = aws_route_table.glb_pri_route.id
+  subnet_id = aws_subnet.glb_pri_sub.id
+  depends_on = [aws_route_table.glb_pri_route]
+}
+resource "aws_eip" "glb_eip" {
+  vpc = true
+}
+resource "aws_nat_gateway" "glb_nat" {
+  allocation_id = aws_eip.glb_eip.id
+  subnet_id = aws_subnet.glb_pub_sub.id
+  depends_on = [aws_vpc.glb_vpc]
+}
+
+resource "aws_security_group" "glb_sg" {
+  name = var.sgname
+  vpc_id = aws_vpc.glb_vpc.id
+  ingress {
+    from_port = 22
+    protocol  = "TCP"
+    to_port   = 22
+    cidr_blocks = var.ipblock
+  }
+  ingress {
+    from_port = 80
+    protocol  = "TCP"
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    protocol  = "TCP"
+    to_port   = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = var.sg-tag
+}
+resource "aws_key_pair" "glb_keypair" {
+  key_name   = "testvibin-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTIvWNXuX50Q2VGQ0gV1/aWIGwySU9/bVvnewYnbHCI0ZANBo8MwAaAhWm4tmcQWl/SheHZDuJ8xHI/bf69x2DVQDP4Bqsrbxvm9kFUo+I0nXpJh3xKlk5pRf8VXX2u9V7vEpmynXYQMWI2PsiyTNYMqC33lgN0HLHUwLdwGcTGbUOeRdXkz5QA0EdO4Qfp2/xNTW4h2iwth+8ChTSVv8xbUcS96vpDyJnc+1ZHXL84aOQN7AESGbeup9dlJw2ydNfzuhz21JeEAlg2Jp7pB+96L55FB0sozw8AfddsSWUvCtASk6tSbCDTjQ5Z3brOXf12rR05/nz6qZqtvXMBW5M+/eql+ZQFusvTvDOAlVfQPfAdkMhEcoOdDRPsUNknkvzyu34UVjT3hAQdAYgDuaMAdcJLigVTwirFqGKHGAl/JTAOjBXzDzi9EFr+QumyvwY34+3aJkY7V6XCldYf73BW4z33HHpJ1XIjgIaBe0linZozdHRR0WhTMlez2ixiE8= HAI@DESKTOP-2KAJDDS"
+}
